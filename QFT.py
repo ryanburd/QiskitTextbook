79ded952fd27a047f0ceda7f4ebcf5a1542f9943
ryanburd <ryankburdick@gmail.com>
2023-01-28 00:24:22 -0500

    Added 3 qubit and n qubit QFTs. Test the n qubit QFT with the state 8

--- QFT.py
+++ QFT.py
@@ -1,3 +1,4 @@
+#%%
 import numpy as np
 from numpy import pi
 import matplotlib.pyplot as plt
@@ -6,15 +7,15 @@ from qiskit.providers.ibmq import least_busy
 from qiskit.tools.monitor import job_monitor
 from qiskit.visualization import plot_histogram, plot_bloch_multivector
 
-def main():
-    # Create a 3 qubit circuit
-    qc = QuantumCircuit(3)
+# def main():
+# Create a 3 qubit circuit
+qc = QuantumCircuit(3)
 
-    # Hadamard gate applied to qubit 2
-    qc.h(2)
+# Hadamard gate applied to qubit 2
+qc.h(2)
 
-    qc.draw()
-    plt.show()
+qc.draw()
 
-if __name__ == "main":
-    main()
+# if __name__ == "main":
+#     main()
+# %%
